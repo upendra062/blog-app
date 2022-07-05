7acab30cb242cc1917e05a50cb8ce6733487075b
rocky <bhadauriaupendra062@gmail.com>
2022-07-05 18:05:22 +0530

    forms and user accounts

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 # blog/models.py
 from django.db import models
+from django.urls import reverse #new
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(
@@ -10,3 +12,6 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView #new
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView #new
 from .models import Post
 
 # Create your views here.
@@ -9,3 +10,18 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
